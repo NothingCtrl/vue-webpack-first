2fb1d0bffd5fa62c43a239dbb5f508117decbe0b
NothingCtrl <nothingctrl@gmail.com>
2017-04-24 16:37:12 +0700

    first commit

---
+++ src/Input.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Binding from Message.vue: {{ msg }}</p>
+        <input type="text" v-model="message"/>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['msg'],
+        data() {
+            return {
+                message: ''
+            }
+        }
+    }
+</script>
